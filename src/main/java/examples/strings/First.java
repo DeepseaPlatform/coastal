74fe430ca8e6100ad92e837ae79bb6afb940c73b
Jaco Geldenhuys <jacogeld@gmail.com>
2018-07-02 00:59:04 +0200

    Add delegate example from DEEPSEA

---
+++ src/main/java/examples/strings/First.java
@@ -0,0 +1,25 @@
+package examples.strings;
+
+public class First {
+
+	public static int test(String str) {
+		if (str.startsWith("bl")) {
+			if (str.endsWith("ue")) {
+				System.out.println("Match");
+				return 0;
+			} else {
+				System.out.println("No match (A)");
+				return 1;
+			}
+		}
+		else {
+			System.out.println("No match (B)");
+			return 2;
+		}
+	}
+	
+	public static void main(String[] args) {
+		test("hell");
+	}
+
+}
