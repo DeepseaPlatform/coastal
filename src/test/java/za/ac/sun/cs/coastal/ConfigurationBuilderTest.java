4b3dff620899c74a6acbe07ca7e06927507190cd
Jaco Geldenhuys <jacogeld@gmail.com>
2018-07-18 06:33:02 +0200

    Write some simple tests for configuration testing

---
+++ src/test/java/za/ac/sun/cs/coastal/ConfigurationBuilderTest.java
@@ -0,0 +1,94 @@
+package za.ac.sun.cs.coastal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import za.ac.sun.cs.coastal.reporting.ReporterManager;
+
+public class ConfigurationBuilderTest {
+
+	@Test
+	public void testMainNotSet() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		final String version = "test";
+		final ReporterManager reporterManager = new ReporterManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder(log, version, reporterManager);
+		assertFalse(cb.isMainSet());
+	}
+
+	@Test
+	public void testMainSet() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		final String version = "test";
+		final ReporterManager reporterManager = new ReporterManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder(log, version, reporterManager);
+		ConfigurationBuilder cb0 = cb.setMain("main");
+		assertEquals(cb, cb0);
+		assertTrue(cb.isMainSet());
+	}
+	
+	@Test
+	public void testStringProperties() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		final String version = "test";
+		final ReporterManager reporterManager = new ReporterManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder(log, version, reporterManager);
+		assertEquals(cb, cb.setArgs("args"));
+		assertEquals(cb, cb.setMain("main"));
+		Configuration c = cb.construct();
+		assertEquals("args", c.getArgs());
+		assertEquals("main", c.getMain());
+	}
+	
+	@Test
+	public void testBooleanProperties() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		final String version = "test";
+		final ReporterManager reporterManager = new ReporterManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder(log, version, reporterManager);
+		assertEquals(cb, cb.setDumpAll(true));
+		assertEquals(cb, cb.setDumpAsm(true));
+		assertEquals(cb, cb.setDumpConfig(true));
+		assertEquals(cb, cb.setDumpFrame(true));
+		assertEquals(cb, cb.setDumpInstrumenter(true));
+		assertEquals(cb, cb.setDumpPaths(true));
+		assertEquals(cb, cb.setDumpTrace(true));
+		assertEquals(cb, cb.setEchoOutput(true));
+		assertEquals(cb, cb.setObeyStops(true));
+		assertEquals(cb, cb.setRecordMarks(true));
+		Configuration c = cb.construct();
+		assertTrue(c.getDumpAll());
+		assertTrue(c.getDumpAsm());
+		assertTrue(c.getDumpConfig());
+		assertTrue(c.getDumpFrame());
+		assertTrue(c.getDumpInstrumenter());
+		assertTrue(c.getDumpPaths());
+		assertTrue(c.getDumpTrace());
+		assertTrue(c.getEchoOutput());
+		assertTrue(c.getObeyStops());
+		assertTrue(c.getRecordMarks());
+	}
+	
+	@Test
+	public void testLongProperties() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		final String version = "test";
+		final ReporterManager reporterManager = new ReporterManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder(log, version, reporterManager);
+		assertEquals(cb, cb.setLimitConjuncts(123));
+		assertEquals(cb, cb.setLimitPaths(234));
+		assertEquals(cb, cb.setLimitRuns(345));
+		assertEquals(cb, cb.setLimitTime(456));
+		Configuration c = cb.construct();
+		assertEquals(123, c.getLimitConjuncts());
+		assertEquals(234, c.getLimitPaths());
+		assertEquals(345, c.getLimitRuns());
+		assertEquals(456, c.getLimitTime());
+	}
+	
+}
