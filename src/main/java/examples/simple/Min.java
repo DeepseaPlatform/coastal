8acfaf6c8de7460d7cc3d44806393082e7911c0e
Jaco Geldenhuys <jacogeld@gmail.com>
2018-07-30 21:49:39 +0200

    Add additional cases for problematic example [ci skip]

--- src/main/java/examples/simple/Min.java
+++ src/main/java/examples/simple/Min.java
@@ -3,8 +3,13 @@ package examples.simple;
 public class Min {
 
 	public static void main(String[] args) {
-		//run(0, 0, 0);
-		run(0, 1, 2);
+		//run(0, 0, 0); // OK, BUT INSERTED PATHS WRONG
+		//run(0, 1, 2); // BROKEN
+		run(0, 2, 1); // OK
+		//run(1, 0, 2); // BROKEN
+		//run(1, 2, 0); // OK
+		//run(2, 0, 1); // BROKEN
+		//run(2, 1, 0); // OK
 		System.out.println("DONE");
 	}
 
