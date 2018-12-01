b4fb33f3980a72b3186e28f60949f3ffde4fab16
Jaco Geldenhuys <jacogeld@gmail.com>
2018-12-01 07:13:37 +0200

    Add more tests for observers [ci-skip]

---
+++ src/main/java/za/ac/sun/cs/coastal/observers/StopControllerFactory.java
@@ -0,0 +1,60 @@
+package za.ac.sun.cs.coastal.observers;
+
+import org.apache.logging.log4j.Logger;
+
+import za.ac.sun.cs.coastal.Banner;
+import za.ac.sun.cs.coastal.COASTAL;
+import za.ac.sun.cs.coastal.messages.Tuple;
+
+public class StopControllerFactory implements ObserverFactory {
+
+	public StopControllerFactory(COASTAL coastal) {
+	}
+
+	@Override
+	public int getFrequency() {
+		return ObserverFactory.ONCE_PER_RUN;
+	}
+
+	@Override
+	public ObserverManager createManager(COASTAL coastal) {
+		return new StopManager(coastal);
+	}
+
+	@Override
+	public Observer createObserver(COASTAL coastal, ObserverManager manager) {
+		return null;
+	}
+	
+	// ======================================================================
+	//
+	// MANAGER FOR STOP CONTROL
+	//
+	// ======================================================================
+	
+	private static class StopManager implements ObserverManager {
+
+		private final Logger log;
+		
+		private final COASTAL coastal;
+
+		public StopManager(COASTAL coastal) {
+			log = coastal.getLog();
+			this.coastal = coastal;
+			coastal.getBroker().subscribe("stop", this::stop);
+		}
+		
+		public void stop(Object object) {
+			Tuple tuple = (Tuple) object;
+			coastal.stopWork();
+			String message = (String) tuple.get(1);
+			if (message == null) {
+				new Banner('!').println("PROGRAM TERMINATION POINT REACHED").display(log);
+			} else {
+				new Banner('!').println("PROGRAM TERMINATION POINT REACHED").println(message).display(log);
+			}
+		}
+
+	}
+
+}
--- src/test/java/za/ac/sun/cs/coastal/SystemTestsObservers.java
+++ src/test/java/za/ac/sun/cs/coastal/SystemTestsObservers.java
@@ -2,6 +2,7 @@ package za.ac.sun.cs.coastal;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.configuration2.ImmutableConfiguration;
 import org.apache.logging.log4j.LogManager;
@@ -56,6 +57,96 @@ public class SystemTestsObservers {
 		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[101]"));
 		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[110]"));
 		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[111]"));
+	}
+	
+	@Test
+	public void testMarkerCoverage02() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		ImmutableConfiguration config = COASTAL.loadConfiguration(log, new String[] { "tests/observers/MarkerCoverage02.xml" });
+		assertNotNull(config);
+		COASTAL coastal = new COASTAL(log, config);
+		coastal.start(false);
+		Reporter reporter = coastal.getReporter();
+		assertEquals(8, reporter.getStatLong("COASTAL.dive-count"));
+		assertEquals(1, reporter.getStatLong("COASTAL.diver-tasks"));
+		assertEquals(1, reporter.getStatLong("COASTAL.strategy-tasks"));
+		assertEquals(7, reporter.getStatLong("Instrumentation.cache-hit-count"));
+		assertEquals(8, reporter.getStatLong("Instrumentation.instrumented-count"));
+		assertEquals(56, reporter.getStatLong("Instrumentation.requests-count"));
+		assertEquals(0, reporter.getStatLong("Strategy.infeasible-count"));
+		assertEquals(8, reporter.getStatLong("Strategy.inserted-paths"));
+		assertEquals(0, reporter.getStatLong("Strategy.revisited-paths"));
+		assertEquals(0, reporter.getStatLong("MarkerCoverage.marker[0]"));
+		assertEquals(8, reporter.getStatLong("MarkerCoverage.marker[1]"));
+		assertEquals(4, reporter.getStatLong("MarkerCoverage.marker[10]"));
+		assertEquals(4, reporter.getStatLong("MarkerCoverage.marker[11]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[100]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[101]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[110]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[111]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1000]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1001]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1010]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1011]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1100]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1101]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1110]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[1111]"));
+	}
+	
+	@Test
+	public void testMarkerCoverage03() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		ImmutableConfiguration config = COASTAL.loadConfiguration(log, new String[] { "tests/observers/MarkerCoverage03.xml" });
+		assertNotNull(config);
+		COASTAL coastal = new COASTAL(log, config);
+		coastal.start(false);
+		Reporter reporter = coastal.getReporter();
+		assertEquals(8, reporter.getStatLong("COASTAL.dive-count"));
+		assertEquals(1, reporter.getStatLong("COASTAL.diver-tasks"));
+		assertEquals(1, reporter.getStatLong("COASTAL.strategy-tasks"));
+		assertEquals(7, reporter.getStatLong("Instrumentation.cache-hit-count"));
+		assertEquals(8, reporter.getStatLong("Instrumentation.instrumented-count"));
+		assertEquals(56, reporter.getStatLong("Instrumentation.requests-count"));
+		assertEquals(0, reporter.getStatLong("Strategy.infeasible-count"));
+		assertEquals(8, reporter.getStatLong("Strategy.inserted-paths"));
+		assertEquals(0, reporter.getStatLong("Strategy.revisited-paths"));
+		assertEquals(0, reporter.getStatLong("MarkerCoverage.marker[never]"));
+		assertEquals(8, reporter.getStatLong("MarkerCoverage.marker[compute-8]"));
+		assertEquals(4, reporter.getStatLong("MarkerCoverage.marker[<5-4]"));
+		assertEquals(4, reporter.getStatLong("MarkerCoverage.marker[>=5-4]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[<5<7-2]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[<5>=7-2]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[>=5<7-2]"));
+		assertEquals(2, reporter.getStatLong("MarkerCoverage.marker[>=5>=7-2]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[<5<7==9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[<5<7!=9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[<5>=7==9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[<5>=7!=9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[>=5<7==9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[>=5<7!=9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[>=5>=7==9-1]"));
+		assertEquals(1, reporter.getStatLong("MarkerCoverage.marker[>=5>=7!=9-1]"));
+	}
+	
+	@Test
+	public void testStopController01() {
+		final Logger log = LogManager.getLogger("COASTAL-TEST");
+		ImmutableConfiguration config = COASTAL.loadConfiguration(log, new String[] { "tests/observers/StopController01.xml" });
+		assertNotNull(config);
+		COASTAL coastal = new COASTAL(log, config);
+		coastal.start(false);
+		Reporter reporter = coastal.getReporter();
+		assertEquals(2, reporter.getStatLong("COASTAL.dive-count"));
+		assertEquals(1, reporter.getStatLong("COASTAL.diver-tasks"));
+		assertEquals(1, reporter.getStatLong("COASTAL.strategy-tasks"));
+		assertEquals(1, reporter.getStatLong("Instrumentation.cache-hit-count"));
+		assertEquals(2, reporter.getStatLong("Instrumentation.instrumented-count"));
+		long requestDelta = Math.abs(reporter.getStatLong("Instrumentation.requests-count") - 12);
+		assertTrue(requestDelta < 4);
+		assertEquals(0, reporter.getStatLong("Strategy.infeasible-count"));
+		assertEquals(1, reporter.getStatLong("Strategy.inserted-paths"));
+		assertEquals(0, reporter.getStatLong("Strategy.revisited-paths"));
 	}
 	
 }
