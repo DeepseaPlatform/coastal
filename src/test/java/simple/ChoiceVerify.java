0182291a87acce3e1a85f7bc7053a322e79cc079
visserw <visserw@10.110.157.204>
2019-11-07 20:55:45 +0200

    checkstyle fixes

--- src/test/java/simple/ChoiceVerify.java
+++ src/test/java/simple/ChoiceVerify.java
@@ -1,43 +1,25 @@
 package simple;
 
-import org.sosy_lab.sv_benchmarks.*;
+import org.sosy_lab.sv_benchmarks.Verifier;
 
 public class ChoiceVerify {
 
-	private static void testSimple() {
-		
-		boolean b1 = Verifier.nondetBoolean();
-		boolean b2 = Verifier.nondetBoolean();
-		
-		if (b1 == b2) {
-			b1 = b2;
-		} else {
-			if (b1) {
-				b1 = true;
-			} else if (b2) {
-				b2 = true;
-			}
-			
-		}
-		System.out.println(" b1 = " + b1 + " b2 = " + b2);
-	}
-	
 	private static void testLoop() {
 		int i = 0;
-		int state=0;
-		
+		int state = 0;
+
 		while (i < 2) {
 			boolean b = Verifier.nondetBoolean();
-			
 			if (state == 0) {
-				if (b)
+				if (b) {
 					state = 1;
-				else
+				} else {
 					state = 2;
-			}
-			else if (state == 1){
-				if (!b)
-					assert false;					
+				}
+			} else if (state == 1) {
+				if (!b) {
+					assert false;
+				}
 			} else {
 				if (b) {
 					assert false;
@@ -46,9 +28,8 @@ public class ChoiceVerify {
 			i++;
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		//testSimple();
 		testLoop();
 	}
 
