6013ede6c965c477a0dae3b458aaf65eefb8a4bb
visserw <visserw@10.110.157.204>
2019-11-07 16:45:56 +0200

    version of minepump with parameters

--- src/examples/java/minepump/Main.java
+++ src/examples/java/minepump/Main.java
@@ -1,14 +1,15 @@
 package minepump;
 
-import minepump.MinePumpSystem.Environment;
-import minepump.MinePumpSystem.MinePump;
 import org.sosy_lab.sv_benchmarks.Verifier;
 
 public class Main {
 
   private static int cleanupTimeShifts = 2;
 
-  public static void main(String[] args) { randomSequenceOfActions(3); }
+  public static void main(String[] args) { 
+	  //randomSequenceOfActions(3); 
+	  randomSequenceOfActions(false, false, false, false, false, false, false, false, false, false, false, false);
+  }
 
   public static boolean getBoolean() { return Verifier.nondetBoolean(); }
 
@@ -25,7 +26,9 @@ public class Main {
       boolean action4 = false;
       if (!action3)
         action4 = getBoolean();
-
+      
+      System.out.println(" counter = " + counter + " a1=" + action1 + " a2=" +action2 + " a3=" +action3 + " a4=" + action4);
+      
       if (action1) {
         a.waterRise();
       }
@@ -47,4 +50,67 @@ public class Main {
       a.timeShift();
     }
   }
+  
+  public static void randomSequenceOfActions(boolean a10, boolean a20, boolean a30, boolean a40,
+		  									 boolean a11, boolean a21, boolean a31, boolean a41,
+		  									 boolean a12, boolean a22, boolean a32, boolean a42) {
+	    Actions a = new Actions();
+
+	    int maxLength = 3;
+	    int counter = 0;
+	    while (counter < maxLength) {
+	      counter++;
+
+	      boolean action1 = false;
+	      boolean action2 = false;
+	      boolean action3 = false;
+	      boolean action4 = false;
+	      
+	      if (counter == 1) {
+	    	  action1 = a10;
+	    	  action2 = a20;
+	    	  action3 = a30;
+	    	  if (!action3)
+	  	        action4 = a40;
+	  	      
+	      } else if (counter == 2) {
+	    	  action1 = a11;
+	    	  action2 = a21;
+	    	  action3 = a31;
+	    	  if (!action3)
+		  	        action4 = a41;
+	      } else {
+	    	  action1 = a12;
+	    	  action2 = a22;
+	    	  action3 = a32;
+	    	  if (!action3)
+		  	        action4 = a42;
+	      }
+	      
+	      
+	      System.out.println(" counter = " + counter + " a1=" + action1 + " a2=" +action2 + " a3=" +action3 + " a4=" + action4);
+	      
+	      if (action1) {
+	        a.waterRise();
+	      }
+
+	      if (action2) {
+	        a.methaneChange();
+	      }
+
+	      if (action3) {
+	        a.startSystem();
+	      } else if (action4) {
+	        a.stopSystem();
+	      }
+
+	      a.timeShift();
+	    }
+
+	    for (counter = 0; counter < cleanupTimeShifts; counter++) {
+	      a.timeShift();
+	    }
+	  }
+
+  
 }
