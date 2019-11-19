13d226ecf6102f6058c4a893c4884a401c268e98
Jaco Geldenhuys <jacogeld@gmail.com>
2019-11-19 18:44:28 +0200

    Fix another error in SVCOMP script

--- src/examples/java/svcomp/Main.java
+++ src/examples/java/svcomp/Main.java
@@ -1,22 +1,20 @@
 package svcomp;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.sosy_lab.sv_benchmarks.Verifier;
-
+/*
+ * Origin of the benchmark:
+ *     license: 4-clause BSD (see /java/jbmc-regression/LICENSE)
+ *     repo: https://github.com/diffblue/cbmc.git
+ *     branch: develop
+ *     directory: regression/cbmc-java/ArithmeticException5
+ * The benchmark was taken from the repo: 24 January 2018
+ */
 public class Main {
-
-	public static void main(String[] args) {
-		Pattern expression = Pattern.compile("W.*\\d[0-35-9]-\\d\\d-\\d\\d");
-
-		String string1 = Verifier.nondetString();
-
-		Matcher matcher = expression.matcher(string1);
-
-		while (matcher.find()) {
-			System.out.println(matcher.group());
-			String tmp = matcher.group();
-			assert tmp.equals("WWWWW's Birthday is 12-17-77");
-		}
-	}
+  public static void main(String args[]) {
+    try {
+      double i = 0;
+      double j = 10 / i;
+    } catch (ArithmeticException exc) {
+      assert false;
+    }
+  }
 }
